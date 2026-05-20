14c4115582ccac471f5ed8fc91589e169bd946a4
Shanuka Wanniarachchi <shanukawanniarachchi2000@gmail.com>
2026-05-20 18:07:29 +0530

    login update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -49,4 +49,55 @@ router.post("/register", async (req, res) => {
   }
 });
 
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+ 
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Email or Password"
+      });
+    }
+
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Email or Password"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error"
+    });
+  }
+});
+
 module.exports = router;
